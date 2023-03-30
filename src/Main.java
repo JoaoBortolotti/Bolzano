9f26deac0f48e11b0df58418e8f84626fbb40761
JoaoBortolotti <joao.bortolotti@edu.unifil.br>
2023-03-30 20:28:15 -0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,48 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    private static Scanner scr = null;
+    
+    private static Scanner scr = new Scanner(System.in);
     
     public static void main(String[] args) {
-        scr = new Scanner(System.in);
-        int a =0;
-        int x = 0;
-        System.out.println("Qual o maior expoente da sua função?");
-        int exp = scr.nextInt();
-        int[] valor = new int[exp];
-        
-        if (exp <= 11) {
-            for (int i = 0; i <= exp; i++) {
-                System.out.println("Insira o valor do x^" + i);
-                valor[i] = scr.nextInt() +(x^i);
-                System.out.println("x^" + i + " = " + valor[i]);
-            }
+        System.out.println("Qual valor máximo do seu expoente?");
+        int exp = scr.nextInt() + 1;
 
-        } else {
-            System.out.println("O expoente deve ser menor ou igual a 10");
+        System.out.println("Qual o expoente do Épsilon da sua função?");
+        System.out.print("10^-");
+        int epsl = scr.nextInt();
+        epsl = -epsl;
+        
+        double valor[] = new double[exp];
+        double variacoes[] = new double[11];
+        
+        /*Pegando os valores dos expoentes*/
+        if(exp <= 11){
+            for (int i = 0; i < valor.length; i++){
+                System.out.println("Insira o valor do x^" + i);
+                valor[i] = scr.nextDouble();                  
+            }
+        }else{
+            System.out.println("O expoente deve ser menor que 11");
+            System.exit(0); 
+        }  
+
+        /*Printando o valor dos expoentes*/
+        for (int i = 0; i < valor.length ; i ++){
+            System.out.println("x^" + i + " = " + valor[i]);
         }
+
+        /*Calculando "X" das Funções*/
+        for(int x = 0; x <= 10; x++){
+            int a = x-5;
+            double resultado = 0;
+            for (int i = 0; i < valor.length; i++){
+                resultado += Math.pow(a, i) * valor[i];
+            }
+            variacoes[x] = resultado;
+            System.out.println("Valor de x = " + a + " : " + variacoes[x]);
+        }
+
+        /*Capturando Intervalos */
     }
 }
